395fed6851918a3cbfeea2b8113beebe62f1e5eb
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-05-05 21:14:42 +0545

    if else statement

--- if_else.py
+++ if_else.py
@@ -1,1 +1,11 @@
-print("Hello World!!")
+base = input("Enter base : ")
+base = int(base)
+height = input("Enter height : ")
+height = int(height)
+area = base * height
+print("Area of triangle = ", area)
+
+if area >= 25:
+    print("Area is greater than 25")
+else:
+    print("Area is smaller than 25")
