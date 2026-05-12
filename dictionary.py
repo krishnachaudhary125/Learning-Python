db91eaa0e52338d41fd36834ada154fdaa4c3d0c
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-05-12 21:38:13 +0545

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+info = {
+    "name": "Krishna Chaudhary",
+    "age": 21,
+    "city": "Ltp"
+}
+
+print(info)
+
+print(info["name"])
+
+print(info.keys())
+
+print(info.values())
+
+info.update({"city": "Lalitpur"})
+
+info.update({"country": "Nepal"})
+
+print(info)
