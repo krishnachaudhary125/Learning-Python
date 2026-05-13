62504b564e03dc977cecf5da3876b2e2778bf6fc
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-05-13 21:35:31 +0545

    match same as switch

---
+++ match.py
@@ -0,0 +1,17 @@
+day = input("Enter a day of the week: ")
+day = int(day)
+match day:
+    case 1:
+        print("Sunday")
+    case 2:
+        print("Monday")
+    case 3:
+        print("Tuesday")
+    case 4:
+        print("Wednesday")
+    case 5:
+        print("Thursday")
+    case 6:
+        print("Friday")
+    case 7:
+        print("Saturday")
