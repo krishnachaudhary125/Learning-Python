b8a5725e6578e5349f9d8cf0cbb9426326c78971
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-05-13 21:29:46 +0545

    nested dictionary

---
+++ nested_dictionary.py
@@ -0,0 +1,12 @@
+dictionary = {
+    "student_one":{
+        "name_one": "Krishna Chaudhary",
+        "addr_one": "Lalitpur"
+    },
+    "student_two":{
+        "name_two": "Subodh Sunuwar",
+        "addr_two": "Bhaktapur"
+    }
+}
+
+print(dictionary)
