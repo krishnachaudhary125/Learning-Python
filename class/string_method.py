138b73abaf451b52f350b761207640414d035518
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-02 09:31:29 +0545

    Use of string methods

---
+++ class/string_method.py
@@ -0,0 +1,17 @@
+txt = "Krishna Chaudhary"
+
+print(txt.strip())			# remove white space
+
+print(txt.upper())			# change text char to uppercase
+
+print(txt.split())			# split into list
+
+print(txt.count("a"))			# count char repeatance
+
+print(txt.lower())			# change text char to lowercase
+
+print(txt.join(["I am ", " and I am 21 years old."]))
+
+print(txt.find("k"))			# find substring index
+
+print(txt.replace("a", "b"))		# replace char
