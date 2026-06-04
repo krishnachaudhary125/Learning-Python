2c748366dd9987bcafb3bc59b1f1ab4e2cbc7279
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-04 07:53:21 +0545

    string validation

---
+++ class/string_validation.py
@@ -0,0 +1,19 @@
+text = "Krishna"
+
+print(text.isalpha())
+
+print(" ".isspace())
+
+print(text.istitle())
+
+print(text.isdigit())
+
+print(text.isupper())
+
+print(text.startswith("Kr"))
+
+print("krishna1".isalnum())
+
+print(text.islower())
+
+print(text.endswith("na"))
