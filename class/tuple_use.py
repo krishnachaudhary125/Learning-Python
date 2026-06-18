4d1c0801accb20b99490a96152555a53f5cbd786
krishnachaudhary125 <krishnachaudhary0125@gmail.com>
2026-06-18 09:21:59 +0545

    tuple use

---
+++ class/tuple_use.py
@@ -0,0 +1,11 @@
+# Tuple creation
+point = (10.5, 20.3)		# 20 coordinate
+rgb = (255, 128, 0)		# color
+person = ("Krishna", 21, "BCA")
+
+# Tuple as dict key (list cant do this)
+locations = ((28.6, 77.2) : "Ltp", (19.0, 72.8) : "Ktm")
+print(locations[(28.6, 77.2)])
+
+# Single - element tuple - needs tralling comme
+print(type(one))
